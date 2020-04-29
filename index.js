278b77c58ddc9ee758994ddfb24adec7352ca282
carlospavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-28 21:52:16 -0300

    Update project to be connected with Firebase

--- index.js
+++ index.js
@@ -73,11 +73,15 @@ firebase.auth().onAuthStateChanged((user) => {
    startRsvpButton.textContent = "LOGOUT";
    // Show guestbook to logged-in users
    guestbookContainer.style.display = "block";
+  // Subscribe to the guestbook collection
+  subscribeGuestbook();
  }
  else{
    startRsvpButton.textContent = "RSVP";
    // Hide guestbook for non-logged-in users
    guestbookContainer.style.display = "none";
+  // Unsubscribe from the guestbook collection
+  unsubscribeGuestbook();
  }
 });
 
@@ -97,3 +101,30 @@ form.addEventListener("submit", (e) => {
  // Return false to avoid redirect
  return false;
 });
+
+// Listen to guestbook updates
+function subscribeGuestbook(){
+   // Create query for messages
+ guestbookListener = firebase.firestore().collection("guestbook")
+ .orderBy("timestamp","desc")
+ .onSnapshot((snaps) => {
+   // Reset page
+   guestbook.innerHTML = "";
+   // Loop through documents in database
+   snaps.forEach((doc) => {
+     // Create an HTML entry for each document and add it to the chat
+     const entry = document.createElement("p");
+     entry.textContent = doc.data().name + ": " + doc.data().text;
+     guestbook.appendChild(entry);
+   });
+ });
+};
+
+// Unsubscribe from guestbook updates
+function unsubscribeGuestbook(){
+ if (guestbookListener != null)
+ {
+   guestbookListener();
+   guestbookListener = null;
+ }
+};
